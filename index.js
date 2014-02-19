c9858e4b82f20b70cf4704943368e1fa4e5641ca
Manuel Alabor <msites@msites.net>
2014-02-19 09:17:48 +0100

    more options. implemented polling

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var request = require('request')
+var debug = require('debug')('traffic-lights')
+	, request = require('request')
 	, prettyjson = require('prettyjson')
+	, Timestring = require('timestring')
 	, config = require('./config.json')
-	, buildServer = require('./src/buildServer/' + config.source.type);
+	, buildServer = require('./src/buildServer/' + config.source.type)
 
-buildServer(config.source, request)
-.then(function(latestBuildState) {
-	console.log(latestBuildState);
-})
-.catch(function(error) {
-	console.error(error);
-});
+function poll() {
+	buildServer(config.source, request)
+	.then(function(latestBuildState) {
+		debug('got build state ' + latestBuildState);
+		console.log(latestBuildState);
+	})
+	.catch(function(error) {
+		debug('error while fetching build state :(');
+		console.error(error);
+	});
+
+	debug('schedule next poll in ' + (config.pollInterval / 1000) + 'seconds');
+	setTimeout(poll, config.pollInterval);
+}
+
+poll();
--- src/buildServer/bamboo.js
+++ src/buildServer/bamboo.js
@@ -1,7 +1,10 @@
-var q = require('q')
+var debug = require('debug')('traffic-lights:buildServer:bamboo')
+	, q = require('q')
 	, states = require('./states');
 
 function latestBuildOnBamboo(config, request) {
+	debug('fetch build from bamboo');
+
 	var defered = q.defer()
 		, url = config.url;
 
@@ -9,6 +12,8 @@ function latestBuildOnBamboo(config, request) {
 		url += '/';
 	}
 	url += 'rest/api/latest/result/IB-IBP.json?max-result=1&os_authType=basic';
+
+	debug('fetching latest build from url');
 
 	request.get(url
 		, {
@@ -20,17 +25,22 @@ function latestBuildOnBamboo(config, request) {
 		}
 		, function(error, response, body) {
 			if(!error) {
+				debug('got valid response');
+
 				var latestBuild = JSON.parse(body).results.result[0]
 					, state = states.OTHER;
 
 				if(latestBuild.state === 'Failed') {
+					debug('build says it failed');
 					state = states.FAILED;
 				} else if(latestBuild.state === 'Successful') {
+					debug('build says it was successful');
 					state = states.SUCCESS;
 				}
 
 				defered.resolve(state);
 			} else {
+				debug('connection error');
 				defered.reject(error);
 			}
 		}
